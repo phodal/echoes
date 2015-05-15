8cacb32f3c689283a1874cc869b0aa30ded2cdec
Fengda HUANG <h@phodal.com>
2015-05-15 23:22:54 +0800

    fix static files issue

--- echoes/urls.py
+++ echoes/urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from core.views import direct_to_template
 from echoes import settings
 
+
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'echoes.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url("^$", direct_to_template, {"template": "index.html"}, name="home"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += patterns("frontend.views",
+   url("^$", "homepage", name="home"),
+)
 
 urlpatterns += staticfiles_urlpatterns()
