cb956e9b8e6fb865c2624c5e51da3fb6f7edb882
Fengda HUANG <h@phodal.com>
2015-05-19 08:25:09 +0800

    add homepage method

--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.template.response import TemplateResponse
 
-def homepage(request):
-    return render_to_response('index.html')
+
+def render(request, templates, dictionary=None, context_instance=None,
+           **kwargs):
+    """
+    Mimics ``django.shortcuts.render`` but uses a TemplateResponse for
+    ``mezzanine.core.middleware.TemplateForDeviceMiddleware``
+    """
+    dictionary = dictionary or {}
+    if context_instance:
+        context_instance.update(dictionary)
+    else:
+        context_instance = RequestContext(request, dictionary)
+    return TemplateResponse(request, templates, context_instance, **kwargs)
+
+
+def homepage(request, **kwargs):
+    context = {"params": kwargs}
+    for (key, value) in context.items():
+        if callable(value):
+            context[key] = value()
+    return render(request, 'index.html', context)
