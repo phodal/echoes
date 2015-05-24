c6ab3d2147b16199024378db9e9ed4a217ab572b
Fengda HUANG <h@phodal.com>
2015-05-24 08:55:59 +0800

    fix timezone issues

--- echoes/urls.py
+++ echoes/urls.py
@@ -2,10 +2,16 @@ from django.conf.urls import include, url, patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.decorators.cache import cache_page
 from blog.feeds import WeblogEntryFeed
+from blog.sitemaps import WeblogSitemap
+from django.contrib.sitemaps import FlatPageSitemap, views as sitemap_views
 
 from conf import settings
 
+sitemaps = {
+    'weblog': WeblogSitemap,
+}
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -19,6 +25,7 @@ urlpatterns += patterns("frontend.views",
 urlpatterns += patterns("core.views",
    url("^set_device/(?P<device>.*)/$", "set_device", name="set_device"),
    url(r'^rss/weblog/$', WeblogEntryFeed(), name='weblog-feed'),
+   url(r'^sitemap\.xml$', cache_page(60 * 60 * 6)(sitemap_views.sitemap), {'sitemaps': sitemaps}),
    url(r'^weblog/', include('blog.urls', namespace='weblog')),
 )
 
