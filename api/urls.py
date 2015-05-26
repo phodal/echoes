01b56431fbf08a442080388943f2db7fe4ff0e33
Fengda HUANG <h@phodal.com>
2015-05-26 20:11:26 +0800

    #[API]# add blog entry api

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 from rest_framework import routers
+from api.views import BlogPostListSet
 
 router = routers.DefaultRouter()
+router.register(r'blog_list', BlogPostListSet)
 
 urlpatterns = patterns('api.views',
     url(r'^', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -1,1 +1,26 @@
-__author__ = 'fdhuang'
+from rest_framework import filters
+from rest_framework import serializers, viewsets
+from rest_framework.decorators import detail_route
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework import renderers
+from blog.models import Entry
+
+
+class BlogpostListSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Entry
+        fields = ('headline', 'slug', 'author')
+
+
+class BlogPostListSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Entry.objects.filter(is_active=True)
+    serializer_class = BlogpostListSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('headline', 'slug', 'author')
+
+    @detail_route(renderer_classes=(renderers.StaticHTMLRenderer,))
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
