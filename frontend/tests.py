24c73917d94d10007d959dca67170df43321dd83
Fengda HUANG <h@phodal.com>
2015-05-30 09:24:15 +0800

    Add test for homepage

--- frontend/tests.py
+++ frontend/tests.py
@@ -4,3 +4,6 @@ class TemplateViewTests(TestCase):
     """
     Tests for views that are instances of TemplateView.
     """
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
