e7790c12f53da931475fd9aaf4ad2af9942c3478
Fengda HUANG <h@phodal.com>
2015-06-01 08:45:15 +0800

    add mobile templates

--- core/middleware.py
+++ core/middleware.py
@@ -6,11 +6,11 @@ class TemplateForDeviceMiddleware(object):
     """
     Inserts device-specific templates to the template list.
     """
+
     def process_template_response(self, request, response):
         if hasattr(response, "template_name"):
             if not isinstance(response.template_name, Template):
-                templates = templates_for_device(request,
-                    response.template_name)
+                templates = templates_for_device(request, response.template_name)
                 response.template_name = templates
         return response
 
